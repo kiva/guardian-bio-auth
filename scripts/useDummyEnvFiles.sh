225c4f7957a0d6793b99372aa3b6502e52e11440
Jeff Kennedy <jeffk@kiva.org>
2020-10-08 14:08:27 -0700

    Typo in useDummyEnvFiles.
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

--- scripts/useDummyEnvFiles.sh
+++ scripts/useDummyEnvFiles.sh
@@ -4,5 +4,5 @@ echo "This script must be run from the top-level guardian-bio-auth directory. If
 
 cp bioanalyzer_service/dummy.env bioanalyzer_service/.env
 cp identity_service/dummy.env identity_service/.env
-cp identity_intelligence_db/dummy.env bioanalyzer_service/.env
-cp identity_template_db/dummy.env bioanalyzer_service/.env
+cp identity_intelligence_db/dummy.env identity_intelligence_db/.env
+cp identity_template_db/dummy.env identity_template_db/.env
