9b8be84d041b55a44dcaa52cd0cbf57e5e145536
Jeff Kennedy <jeffk@kiva.org>
2020-10-15 10:55:31 -0700

    Add exit codes
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

--- scripts/useDummyEnvFiles.sh
+++ scripts/useDummyEnvFiles.sh
@@ -7,6 +7,8 @@ then
   cp identity_intelligence_db/dummy.env identity_intelligence_db/.env
   cp identity_template_db/dummy.env identity_template_db/.env
   echo "Success"
+  exit 0
 else
   echo "This script must be run from the top-level guardian-bio-auth directory. You are currently running it from: $PWD"
+  exit 1
 fi
