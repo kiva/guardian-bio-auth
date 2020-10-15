c962c83882722e4b845b5e290f1a80008d6b8505
Jeff Kennedy <jeffk@kiva.org>
2020-10-15 11:18:53 -0700

    Add a help command and better parsing of command line parameters.
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

--- scripts/useDummyEnvFiles.sh
+++ scripts/useDummyEnvFiles.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-if [[ "$1" == "-f" || "$1" == "--force" || "$PWD" == */guardian-bio-auth ]];
+# Parse command line parameters, if any
+HELP=false
+FORCE=false
+while [[ $# -gt 0 ]]
+do
+  if [[ "$1" == "-h" || "$1" == "--help" ]]
+  then
+    HELP=true
+  elif [[ "$1" == "-f" || "$1" == "--force" ]]
+  then
+    FORCE=true
+  fi
+  shift
+done
+
+# Execute. If help is requested, don't actually execute the script.
+if [[ "$HELP" == true ]]
+then
+  echo "Apply dummy environment variables to each of the repos contained in guardian-bio-auth"
+  echo ""
+  echo "USAGE:  useDummyEnvFiles.sh [OPTIONS]"
+  echo ""
+  echo "Options:"
+  echo "-h, --help     Display this help message"
+  echo "-f, --force    Force execute the useDummyEnvFiles.sh script without checking the current working directory"
+elif [[ "$FORCE" == true || "$PWD" == */guardian-bio-auth ]]
 then
   cp bioanalyzer_service/dummy.env bioanalyzer_service/.env
   cp identity_service/dummy.env identity_service/.env
