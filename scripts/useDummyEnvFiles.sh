76864c2b55dad5e876205189c6f6e870ef908e15
Jeff Kennedy <jeffk@kiva.org>
2020-10-15 10:52:23 -0700

    Fix messaging on useDummyEnvFiles.sh to be a bit more clear and less foreboding.
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

--- scripts/useDummyEnvFiles.sh
+++ scripts/useDummyEnvFiles.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-echo "This script must be run from the top-level guardian-bio-auth directory. If you run it from anywhere else, it will fail."
-
-cp bioanalyzer_service/dummy.env bioanalyzer_service/.env
-cp identity_service/dummy.env identity_service/.env
-cp identity_intelligence_db/dummy.env identity_intelligence_db/.env
-cp identity_template_db/dummy.env identity_template_db/.env
+if [[ "$PWD" == */guardian-bio-auth ]];
+then
+  cp bioanalyzer_service/dummy.env bioanalyzer_service/.env
+  cp identity_service/dummy.env identity_service/.env
+  cp identity_intelligence_db/dummy.env identity_intelligence_db/.env
+  cp identity_template_db/dummy.env identity_template_db/.env
+  echo "Success"
+else
+  echo "This script must be run from the top-level guardian-bio-auth directory. You are currently running it from: $PWD"
+fi
