9a1991e8eb24486e0f712db7d9b3bac6c341e763
Jeff Kennedy <jeffk@kiva.org>
2020-12-17 08:09:38 -0800

    Try 5 minutes? Also fail early for testing - move the healthcheck up
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

--- scripts/healthstack.sh
+++ scripts/healthstack.sh
@@ -3,8 +3,8 @@ set -ev
 
 # This script waits for the bio auth stack to be up
 
-# wait 3 minutes for identity-service to be up
-./scripts/healthcheck.sh http://localhost:8081/healthz 180
+# wait 5 minutes for identity-service to be up
+./scripts/healthcheck.sh http://localhost:8081/healthz 300
 
 # wait 90 seconds for bioanalyzer-service to be up
 ./scripts/healthcheck.sh http://localhost:8089/healthz
