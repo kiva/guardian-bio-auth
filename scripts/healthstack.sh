feb6e54d4c4d7b91a16509f692f5e0a33928ea47
Jeff Kennedy <jeffk@kiva.org>
2020-12-16 17:40:55 -0800

    Identity Service sometimes needs a little bit more time. Extend the healthcheck.
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

--- scripts/healthstack.sh
+++ scripts/healthstack.sh
@@ -3,8 +3,8 @@ set -ev
 
 # This script waits for the bio auth stack to be up
 
-# wait for identity-service to be up
-./scripts/healthcheck.sh http://localhost:8081/healthz
+# wait 3 minutes for identity-service to be up
+./scripts/healthcheck.sh http://localhost:8081/healthz 180
 
-# wait for bioanalyzer-service to be up
+# wait 90 seconds for bioanalyzer-service to be up
 ./scripts/healthcheck.sh http://localhost:8089/healthz
