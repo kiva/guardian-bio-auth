a4568fa6c892a21fac4613f11d08acd9f436297b
Jeff Kennedy <jeffk@kiva.org>
2021-04-27 15:35:19 -0700

    Refactor DB bootstrap logic.
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

--- bio_auth_service/src/main/kotlin/Application.kt
+++ bio_auth_service/src/main/kotlin/Application.kt
@@ -7,12 +7,12 @@ import io.ktor.features.ContentNegotiation
 import io.ktor.serialization.json
 import io.ktor.server.netty.EngineMain
 import io.ktor.util.KtorExperimentalAPI
-import org.kiva.bioauthservice.db.initDB
+import org.kiva.bioauthservice.db.bootstrapDB
 import org.kiva.bioauthservice.errors.registerErrorHandler
 import org.kiva.bioauthservice.fingerprint.registerFingerprintRoutes
 import org.kiva.bioauthservice.routes.registerAppRoutes
 
-// Uses initDB, which itself uses an experimental API
+// Uses bootstrapDB, which itself uses an experimental API
 @KtorExperimentalAPI
 fun Application.module() {
 
@@ -20,7 +20,7 @@ fun Application.module() {
         json()
     }
 
-    val dbPort = initDB(log)
+    val dbPort = bootstrapDB(log)
 
     registerErrorHandler(log)
 
---
+++ bio_auth_service/src/main/kotlin/db/DbBootstrap.kt
@@ -0,0 +1,50 @@
+package org.kiva.bioauthservice.db
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import io.ktor.application.Application
+import io.ktor.config.ApplicationConfig
+import io.ktor.util.KtorExperimentalAPI
+import org.flywaydb.core.Flyway
+import org.jdbi.v3.core.Jdbi
+import org.jdbi.v3.core.kotlin.KotlinPlugin
+import org.kiva.bioauthservice.util.getLong
+import org.kiva.bioauthservice.util.getString
+import org.slf4j.Logger
+
+// ApplicationConfig utils use an experimental API
+@KtorExperimentalAPI
+private fun ApplicationConfig.toHikariConfig(): HikariConfig {
+    val config = HikariConfig()
+    config.jdbcUrl = this.getString("url")
+    config.username = this.getString("username")
+    config.password = this.getString("password")
+    config.connectionTimeout = this.getLong("connectionTimeout")
+    config.idleTimeout = this.getLong("idleTimeout")
+    config.maxLifetime = this.getLong("maxLifetime")
+    return config
+}
+
+// Uses toHikariAPI, which itself uses an experimental API. Plus getting a child config is an experimental API, subject to future renaming.
+@KtorExperimentalAPI
+fun Application.bootstrapDB(logger: Logger): DbPort {
+
+    // Set up Hikari connection pool
+    val baseConfig = environment.config.config("db")
+    val ds = HikariDataSource(baseConfig.toHikariConfig())
+
+    // Set up Flyway
+    val flyway: Flyway = Flyway.configure().dataSource(ds).load()
+    if (flyway.info().pending().isNotEmpty()) {
+        logger.info("Starting to run migrations")
+        flyway.migrate()
+        logger.info("Migrations done")
+    } else {
+        logger.info("No migrations to run")
+    }
+
+    // Set up JDBI
+    val jdbi = Jdbi.create(ds)
+    jdbi.installPlugin(KotlinPlugin())
+    return DbPort(jdbi)
+}
