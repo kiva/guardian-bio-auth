5b9ca06a73c43e900739ba72e8069a9face68379
Jeff Kennedy <jeffk@kiva.org>
2021-05-05 14:37:17 -0700

    Remove unused fun in StringUtils
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

--- bio_auth_service/src/main/kotlin/common/utils/StringUtils.kt
+++ bio_auth_service/src/main/kotlin/common/utils/StringUtils.kt
@@ -1,7 +1,5 @@
 package org.kiva.bioauthservice.common.utils
 
-import org.apache.commons.codec.binary.Hex
-import org.kiva.bioauthservice.common.errors.impl.ImageDecodeException
 import java.util.Base64
 
 /**
@@ -9,13 +7,4 @@ import java.util.Base64
  */
 fun String.base64ToByte(): ByteArray {
     return Base64.getDecoder().decode(this)
-}
-
-/**
- * Convert a hex-encoded string to a ByteArray
- *
- * @throws ImageDecodeException if error happens in decoding the hex formatted image.
- */
-fun String.decodeHex(): ByteArray {
-    return Hex.decodeHex(this.trim().removePrefix("0x").removePrefix("\\x"))
 }
