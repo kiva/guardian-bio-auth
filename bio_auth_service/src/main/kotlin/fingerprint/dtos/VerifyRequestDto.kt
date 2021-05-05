7b5e3113761c46270f4677d9327c0a7c4ef64f69
Jeff Kennedy <jeffk@kiva.org>
2021-05-05 13:14:21 -0700

    Slight change to the way VerifyRequestDto is defined. Makes serialization and validation easier.
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

--- bio_auth_service/src/main/kotlin/fingerprint/dtos/VerifyRequestDto.kt
+++ bio_auth_service/src/main/kotlin/fingerprint/dtos/VerifyRequestDto.kt
@@ -22,17 +22,6 @@ data class VerifyRequestDto(
 
     /**
      * Filter to reduce the amount of prints we need to perform fingerprint matching against.
-     *
-     * Examples include:
-     *
-     * filter by nationalId
-     * filters.put("nationalId", "123456")
-     *
-     * filter by voterId
-     * filters.put("voterId", "123456")
-     *
-     * filter by comma-separated list of DIDs
-     * filters.put("dids", "abcd1234,efgh5678")
      */
     val filters: VerifyRequestFiltersDto,
 
@@ -40,7 +29,8 @@ data class VerifyRequestDto(
      * Defines the type of submitted fingerprint which is either image or template
      */
 //    @NotEmpty
-    val imageType: DataType = DataType.IMAGE,
+    val imageType: DataType = DataType.IMAGE
+) {
 
     /**
      * Params passed to the backend store which are used to authenticate the verification request. Requires an entry for
@@ -48,11 +38,5 @@ data class VerifyRequestDto(
      *
      * While still supporting specifying these as separate top-level fields, this is optional.
      */
-    var params: VerifyRequestParamsDto = VerifyRequestParamsDto("", FingerPosition.RIGHT_INDEX)
-) {
-    init {
-        if (params.image.isEmpty()) {
-            params = VerifyRequestParamsDto(image ?: "", position ?: FingerPosition.RIGHT_INDEX)
-        }
-    }
+    val params: VerifyRequestParamsDto = VerifyRequestParamsDto(image ?: "", position ?: FingerPosition.RIGHT_INDEX)
 }
