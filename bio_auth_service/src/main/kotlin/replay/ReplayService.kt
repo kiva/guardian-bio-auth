3a48d09b19895c7b2b52300da2c9abb44a47af8e
Jeff Kennedy <jeffk@kiva.org>
2021-05-07 16:23:55 -0700

    Add ReplayService tests. Fix in ReplayService to not enable go to the database if replay protection is disabled.
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

--- bio_auth_service/src/main/kotlin/replay/ReplayService.kt
+++ bio_auth_service/src/main/kotlin/replay/ReplayService.kt
@@ -8,8 +8,8 @@ import org.slf4j.Logger
 @KtorExperimentalAPI
 class ReplayService(private val logger: Logger, private val replayRepository: ReplayRepository, private val replayConfig: ReplayConfig) {
     fun checkIfReplay(bytes: ByteArray) {
-        val replay = replayRepository.addReplay(bytes)
         if (replayConfig.enabled) {
+            val replay = replayRepository.addReplay(bytes)
             if (replay.countSeen > 1) {
                 logger.warn("Possible replay attack seen at ${replay.timeSeen}. Image has been used ${replay.countSeen} times.")
             } else {
